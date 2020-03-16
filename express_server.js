d085bc68a0459c83c6189cb80a3422beff62377a
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-16 16:17:25 -0700

    Updated generateRandomString with more letters

--- express_server.js
+++ express_server.js
@@ -12,7 +12,9 @@ const urlDatabase = {
 };
 
 const generateRandomString = () => {
-  const possibleLetters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd' ];
+  const possibleLetters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z' ];
+
   let stringLength = 6;
   let newString = '';
   for (let i = 0; i < stringLength; i++) {
