1bf5294a79e145da0fc32dcb1c810f62e2c76f07
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-16 18:44:06 -0700

    Added server side route and delete url code

--- express_server.js
+++ express_server.js
@@ -52,6 +52,11 @@ app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
