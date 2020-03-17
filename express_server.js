3f4d40046f951f1d302ee3edf1f9c7cd8284da49
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-16 19:16:05 -0700

    Added Post route to edit long Urls

--- express_server.js
+++ express_server.js
@@ -62,6 +62,12 @@ app.get('/urls/:shortURL', (req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls/:shortURL', (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  console.log(req.body);
+  res.redirect('/urls');
+});
+
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
 
