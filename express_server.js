f28137704f21eb407a92debce48166bff1d6a956
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-18 18:29:40 -0700

    Refactored generateRandomString function

--- express_server.js
+++ express_server.js
@@ -52,10 +52,9 @@ const getUserFromEmail = (userDatabase, emailAddress) => {
 
 
 const generateRandomString = () => {
-  const possibleLetters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z' ]; /// add upper case, change to string
-
-    //uuid or guid -- research
+  const possibleLetters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  
+  //uuid or guid -- research
 
   let stringLength = 6;
   let newString = '';
