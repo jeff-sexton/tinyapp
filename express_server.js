95c3644a69bbe04690ac8c4e9d9ab10962b721db
Jeff Sexton <jsexton@Jeffs-MacBook-Pro.local>
2020-03-18 20:36:44 -0700

    Cleanup of unused cookie-parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-
-// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
-
 const bcrypt = require('bcrypt');
 const morgan = require('morgan');
 
@@ -13,7 +10,6 @@ const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2'],
